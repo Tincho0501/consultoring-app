df573b8e497681e57f7028d1f01a3549617670b9
Tincho0501 <tincho0501@gmail.com>
2024-09-10 12:27:47 -0400

    Add Firebase Hosting

--- consultoring-app/src/app/app.component.spec.ts
+++ consultoring-app/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cli-inmueble-app'`, () => {
+  it(`should have as title 'consultoring-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cli-inmueble-app');
+    expect(app.title).toEqual('consultoring-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('cli-inmueble-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('consultoring-app app is running!');
   });
 });
--- consultoring-app/src/environments/environment.ts
+++ consultoring-app/src/environments/environment.ts
@@ -7,12 +7,12 @@ export const environment = {
   name: 'dev',
   firebase: {
     config : {
-      apiKey: "AIzaSyBljiaFUG4xuAui1CfZqMCvrLYVndKNUI0",
-      authDomain: "edificacion-app-9e206.firebaseapp.com",
-      projectId: "edificacion-app-9e206",
-      storageBucket: "edificacion-app-9e206.appspot.com",
-      messagingSenderId: "399997171899",
-      appId: "1:399997171899:web:0d55ade49b8c93ecb6119e"
+      apiKey: 'AIzaSyDPkBpAbHVWXtmKaPGbVab4lG_zWZPfP_4',
+      authDomain: 'tincho0501-consultoring-app.firebaseapp.com',
+      projectId: 'tincho0501-consultoring-app',
+      storageBucket: 'tincho0501-consultoring-app.appspot.com',
+      messagingSenderId: '875715624706',
+      appId: '1:875715624706:web:95ce4a93f40ae4d69d8c1b',
     }
   },
   url: 'http://localhost:5000/'
